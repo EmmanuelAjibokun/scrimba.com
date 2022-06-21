23acdf2031880b765e1ad9ea0742846aa4687539
manny300 <96999656+manny300@users.noreply.github.com>
2022-06-21 09:40:15 +0100

    reset the counter when the saved button is clicked

--- passenger-count/index.js
+++ passenger-count/index.js
@@ -7,10 +7,12 @@ let count = 0
 
 function increment() {
     count += 1
-    countEl.innerText = count
+    countEl.textContent = count
 }
 
 function save() {
     let saved = count + ' - ';
     saveEl.textContent += saved;
+    countEl.textContent = 0;
+    count = 0;
 }
