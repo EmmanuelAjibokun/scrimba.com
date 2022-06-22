759e1dc712736dc0bab3457d06ce437cddb4471a
manny300 <96999656+manny300@users.noreply.github.com>
2022-06-22 18:23:58 +0100

    done with html and css

---
+++ Basketball Scoreboard/index.js
@@ -0,0 +1,26 @@
+const plus1 = document.querySelectorAll('.plus1');
+const plus2 = document.querySelectorAll('.plus2');
+const plus3 = document.querySelectorAll('.plus3');
+let homeScore = document.querySelectorAll('.home-score').textContent;
+let guestScore = document.querySelectorAll('.guest-score').textContent;
+
+function updateHomeScore(homeScore) {
+  document.querySelector('.home-score').textContent = homeScore;
+}
+
+function updateGuestScore(score) {
+  document.querySelector('.home-guest').textContent = score;
+  console.log(score)
+}
+
+function add1() {
+  homeScore = parseInt(homeScore)
+  homeScore = homeScore + 1;
+  updateHomeScore(homeScore)
+  console.log(homeScore)
+}
+
+
+plus1.forEach(button => {
+  button.addEventListener('click', add1)
+})
