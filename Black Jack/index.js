05defd8532fa8aa8665e5994731fea4875cba11a
manny300 <96999656+EmmanuelAjibokun@users.noreply.github.com>
2022-07-02 09:02:28 +0100

    update the sum-el paragraph with the most recent sum

--- Black Jack/index.js
+++ Black Jack/index.js
@@ -1,21 +1,24 @@
-let firstCard = 3;
-let secondCard = 5;
+let firstCard = 10;
+let secondCard = 11;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
 console.log(messageEl)
 
 function startGame() {
+  sumEl.textContent = `Sum: ${sum}`;
   if(sum <= 20) {
-    message = "Do you want to draw a new card? 🙂";
+    message = "Do you want to draw a new card?";
   } else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳";
+    message = "Wohoo! You've got Blackjack!";
     hasBlackJack = true;
   } else {
-    message = "You're out of the game! 😭";
+    message = "You're out of the game!";
     isAlive = false;
   }
   messageEl.textContent = message;
+  console.log(sumEl)
 }
