1fbc81efe5e7f3232cd81f218e657244c54ff230
manny300 <96999656+EmmanuelAjibokun@users.noreply.github.com>
2022-07-02 08:44:30 +0100

    update #message-el to message

--- Black Jack/index.js
+++ Black Jack/index.js
@@ -4,6 +4,8 @@ let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+let messageEl = document.getElementById("message-el");
+console.log(messageEl)
 
 function startGame() {
   if(sum <= 20) {
@@ -15,5 +17,5 @@ function startGame() {
     message = "You're out of the game! 😭";
     isAlive = false;
   }
-  console.log(message)
+  messageEl.textContent = message;
 }
