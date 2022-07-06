31dce57dd1646ac5e791b91f178b8fda4d5a8fd0
manny300 <96999656+EmmanuelAjibokun@users.noreply.github.com>
2022-07-06 20:54:18 +0100

    added a new paragraph for the player's detail and created and object for the player

--- Black Jack/index.js
+++ Black Jack/index.js
@@ -1,4 +1,7 @@
-
+let player = {
+  name: "Emmanuel",
+  chips: 100
+};
 let cards = [];
 let sum = 0;
 let hasBlackJack = false;
@@ -7,6 +10,8 @@ let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": $" + player.chips;
 
 function startGame() {
   isAlive = true;
