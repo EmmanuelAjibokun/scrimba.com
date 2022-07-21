64745009c11c59574fe9fcf0f5542e969042d281
manny300 <96999656+EmmanuelAjibokun@users.noreply.github.com>
2022-07-21 20:46:08 +0100

    using the innerHTML method to add html tags to page on javascript

--- chrome extension/app.js
+++ chrome extension/app.js
@@ -1,10 +1,15 @@
-let myLeads = [];
+let myLeads = ["manny", "www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 const inputEl = document.getElementById("input-el");
 const inputButton = document.getElementById('input-btn');
+let ulEl = document.getElementById("ul-el");
 
 function leadSave() {
   myLeads.push(inputEl.value)
-  console.log(myLeads)
+  return myLeads
 }
 
 inputButton.addEventListener('click', leadSave)
+
+for (let i = 0; i < myLeads.length; i++) {
+  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+}
